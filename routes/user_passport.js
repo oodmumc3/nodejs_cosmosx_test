f5ad4a26bd2f5ffff8706b7a9e8a1ca4fc194a59
kasreder <user@email.mail>
2019-11-24 18:49:15 +0900

    에러수정

--- routes/user_passport.js
+++ routes/user_passport.js
@@ -10,18 +10,16 @@ module.exports = function(router, passport) {
 
     // 홈 화면
     router.route('/').get(function(req, res) {
-        console.log('/ 패스 요청됨.');
-
-        console.log('req.user의 정보');
-        console.dir(req.user);
+        console.log('/ 패스 요청됨. __홈 화면조회시');
+        console.log('req.user의 정보 __홈 화면조회시');
 
         // 인증 안된 경우
         if (!req.user) {
-            console.log('사용자 인증 안된 상태임.');
+            console.log('사용자 인증 안된 상태임. __홈 화면조회시');
             res.render('aaa.ejs', {login_success:false});
 
         } else {
-            console.log('사용자 인증된 상태임.');
+            console.log('사용자 인증된 상태임. __홈 화면조회시');
             if (Array.isArray(req.user)) {
                 res.render('aaa.ejs', {user: req.user[0]._doc}, {login_success:true});
             } else if (Array.isArray(req.user)) {
@@ -34,52 +32,50 @@ module.exports = function(router, passport) {
     
     // 로그인 화면
     router.route('/login').get(function(req, res) {
-        console.log('/login 패스 요청됨.');
+        console.log('/login 패스 요청됨. __로그인 화면조회시');
         if (req.user) {
-            console.log('사용자 인증 된 상태임.');
+            console.log('사용자 인증 된 상태임. __로그인 화면조회시');
             res.redirect('/');
         } else {
-            console.log('사용자 인증 안된 상태임.');
+            console.log('사용자 인증 안된 상태임. __로그인 화면조회시');
             res.render('login.ejs', {message: req.flash('loginMessage')});
         }
     });
      
     // 회원가입 화면
     router.route('/signup').get(function(req, res) {
-        console.log('/signup 패스 요청됨.');
+        console.log('/signup 패스 요청됨. __사이트가입 화면조회시');
         res.render('signup.ejs', {message: req.flash('signupMessage')});
     }); 
     
     // 프로필 화면
     router.route('/profile').get(function(req, res) {
-        console.log('/profile 패스 요청됨.');
+        console.log('/profile 패스 요청됨. __개인정보 화면조회시');
 
         // 인증된 경우, req.user 객체에 사용자 정보 있으며, 인증안된 경우 req.user는 false값임
-        console.log('req.user 객체의 값');
-        console.dir(req.user);
+        console.log('req.user 객체의 값 __개인정보 화면조회시');
 
         // 인증 안된 경우
         if (!req.user) {
-            console.log('사용자 인증 안된 상태임.');
+            console.log('사용자 인증 안된 상태임. __개인정보 화면조회시');
             res.redirect('/');
         } else {
-            console.log('사용자 인증된 상태임.');
-            console.log('/profile 패스 요청됨1111.');
-            console.dir(req.user);
+            console.log('사용자 인증된 상태임. __개인정보 화면조회시');
+            console.log('/profile 패스 요청됨. __개인정보 화면조회시');
 
             if (Array.isArray(req.user)) {
                 res.render('profile.ejs', {user: req.user[0]._doc});
-                res.render('profile.ejs', {login_success:true});
+                //res.render('profile.ejs', {login_success:true});
             } else  {
                 res.render('profile.ejs', {user: req.user});
-                res.render('profile.ejs', {login_success:true});
+                //res.render('profile.ejs', {login_success:true});
             }
         }   
     });
 	
     // 로그아웃
     router.route('/logout').get(function(req, res) {
-        console.log('/logout 패스 요청됨.');
+        console.log('/logout 패스 요청됨. __개인정보 화면조회시');
         req.logout();
         res.redirect('/');
         res.render('aaa.ejs', {login_success:false});
